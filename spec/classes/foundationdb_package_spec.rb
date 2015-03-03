44012748206aa2b5c11ce5658c7c4a4450ed9bf3
Rob Rankin <github@undertow.ca>
2015-03-03 11:55:45 +0000

    cleaning up the tests

--- spec/classes/foundationdb_package_spec.rb
+++ spec/classes/foundationdb_package_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 
 describe 'foundationdb::package' do
   let(:node) { 'test.example.com' }
-  let(:params) { { :package_source => 'http://foundationdb.com' } }
+  let(:osfamily) { 'RedHat' }
+  let(:params) { { :package_source => 'http://foundationdb.com',
+                   :package_name => 'foundationdb',
+                   :manage_repo => true } }
 
   it { is_expected.to compile.with_all_deps }
   it { is_expected.to contain_class('foundationdb::package::redhat') }
